af4492cdceb3889ed8fd36d42424837ce4a59278
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 16:26:20 +0200

    test: apple devient appleay (rouge)

---
+++ pig-latin.test.ts
@@ -0,0 +1,5 @@
+import { assertEquals } from "jsr:@std/assert"
+import { pigLatin } from "./pig-latin.ts"
+Deno.test("apple devient appleay", () => {
+  assertEquals(pigLatin("apple"), "appleay");
+});
