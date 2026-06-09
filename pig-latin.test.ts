f5cbe1838e1c11496f37d6967498b95a2c8cd4d1
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 17:18:08 +0200

    test: igloo devient iglooay (rouge)

--- pig-latin.test.ts
+++ pig-latin.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "jsr:@std/assert"
 import { pigLatin } from "./pig-latin.ts"
-Deno.test("ear devient earay", () => {
-  assertEquals(pigLatin("ear"), "earay");
+Deno.test("igloo devient iglooay", () => {
+  assertEquals(pigLatin("igloo"), "iglooay");
 });
