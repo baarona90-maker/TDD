c7fee95736a98a5c531b7e5b2871591f5c67ae9a
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 19:18:18 +0200

    test: 14 devient XIV (rouge)

--- 03-roman-numerals/src/roman-numerals.test.ts
+++ 03-roman-numerals/src/roman-numerals.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert"
 import { toRoman } from "./roman-numerals.ts"
 
-Deno.test("9 devient IX", () => {
-  assertEquals(toRoman(9), "IX");
+Deno.test("14 devient XIV", () => {
+  assertEquals(toRoman(14), "XIV");
 });
