ad33c946a415260c58aeb7fbfbc8e4aaea5535b6
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 18:55:47 +0200

    feat: lever une erreur si nombre négatif (vert)

--- 02-string-calculator/src/string-calculator.ts
+++ 02-string-calculator/src/string-calculator.ts
@@ -3,5 +3,13 @@ const sum = (acc: number, n: number): number => acc + n;
 
 export function add(numbers: string): number {
   if (numbers === "") return 0;
-  return numbers.split(",").map(toInt).reduce(sum, 0);
+
+  const nums = numbers.split(",").map(toInt);
+  const negatives = nums.filter(n => n < 0);
+
+  if (negatives.length > 0) {
+    throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
+  }
+
+  return nums.reduce(sum, 0);
 }
