d3bec1361e9e2d8f486b535239f3ba3637b74f23
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 19:14:49 +0200

    test: 5 devient V (rouge)

--- 03-roman-numerals/src/roman-numerals.test.ts
+++ 03-roman-numerals/src/roman-numerals.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert"
 import { toRoman } from "./roman-numerals.ts"
 
-Deno.test("4 devient IV", () => {
-  assertEquals(toRoman(4), "IV");
+Deno.test("5 devient V", () => {
+  assertEquals(toRoman(5), "V");
 });
