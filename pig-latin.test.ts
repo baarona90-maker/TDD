7ad4f15fc1d7b3cdd8d615e75914c5ba1fda5412
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 17:28:03 +0200

    test: glove devient oveglay (rouge)

--- pig-latin.test.ts
+++ pig-latin.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "jsr:@std/assert"
 import { pigLatin } from "./pig-latin.ts"
-Deno.test("three devient eethray", () => {
-  assertEquals(pigLatin("three"), "eethray");
+Deno.test("glove devient oveglay", () => {
+  assertEquals(pigLatin("glove"), "oveglay");
 });
