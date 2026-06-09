a1859848cdbddddfddd9c6f3a03b933218e533a1
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 19:22:47 +0200

    test: 1994 devient MCMXCIV (rouge)

--- 03-roman-numerals/src/roman-numerals.test.ts
+++ 03-roman-numerals/src/roman-numerals.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert"
 import { toRoman } from "./roman-numerals.ts"
 
-Deno.test("42 devient XLII", () => {
-  assertEquals(toRoman(42), "XLII");
+Deno.test("1994 devient MCMXCIV", () => {
+  assertEquals(toRoman(1994), "MCMXCIV");
 });
