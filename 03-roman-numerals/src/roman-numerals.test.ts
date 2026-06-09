a3dae5616c1cadc854869da9c47507033093fca3
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 19:13:43 +0200

    test: 4 devient IV (rouge)

--- 03-roman-numerals/src/roman-numerals.test.ts
+++ 03-roman-numerals/src/roman-numerals.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert"
 import { toRoman } from "./roman-numerals.ts"
 
-Deno.test("3 devient III", () => {
-  assertEquals(toRoman(3), "III");
+Deno.test("4 devient IV", () => {
+  assertEquals(toRoman(4), "IV");
 });
