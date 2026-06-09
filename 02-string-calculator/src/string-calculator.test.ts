4c5eb837616ade49c51533069bc2abd08c3a93a0
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 18:58:12 +0200

    test: plusieurs négatifs listés dans l'erreur (rouge)

--- 02-string-calculator/src/string-calculator.test.ts
+++ 02-string-calculator/src/string-calculator.test.ts
@@ -1,10 +1,10 @@
 import { assertEquals, assertThrows } from "jsr:@std/assert"
 import { add } from "./string-calculator.ts"
 
-Deno.test("un nombre négatif lance une erreur", () => {
+Deno.test("plusieurs négatifs dans le message d'erreur", () => {
   assertThrows(
-    () => add("1,-2,3"),
+    () => add("1,-2,-5"),
     Error,
-    "negatives not allowed: -2"
+    "negatives not allowed: -2, -5"
   );
 });
