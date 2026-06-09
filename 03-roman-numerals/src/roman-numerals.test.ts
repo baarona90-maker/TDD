fa195054dbd36c5203b4e69e9e24bda8686e674d
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 19:21:35 +0200

    test: 42 devient XLII (rouge)

--- 03-roman-numerals/src/roman-numerals.test.ts
+++ 03-roman-numerals/src/roman-numerals.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert"
 import { toRoman } from "./roman-numerals.ts"
 
-Deno.test("14 devient XIV", () => {
-  assertEquals(toRoman(14), "XIV");
+Deno.test("42 devient XLII", () => {
+  assertEquals(toRoman(42), "XLII");
 });
