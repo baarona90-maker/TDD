856b1eb258ab3fa40afbe21e8a96f3bce508c83e
unknown <mahamba@esp.sngit config --global user.name>
2026-06-09 18:48:52 +0200

    test: un seul nombre retourne sa valeur (rouge)

--- 02-string-calculator/src/string-calculator.test.ts
+++ 02-string-calculator/src/string-calculator.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals, assertThrows } from "jsr:@std/assert"
 import { add } from "./string-calculator.ts"
 
-Deno.test("chaîne vide retourne 0", () => {
-  assertEquals(add(""), 0);
+Deno.test("un seul nombre retourne ce nombre", () => {
+  assertEquals(add("5"), 5);
 });
